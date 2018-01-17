d701bb23f00b4abdb20461f7e6267da0d89c375c
Campbell Jones <serebitmail@gmail.com>
2018-01-16 20:40:28 -0500

    Added test for multiple log statements to the same file

--- src/test/kotlin/FileWriterTest.kt
+++ src/test/kotlin/FileWriterTest.kt
@@ -1,23 +1,29 @@
-import com.serebit.loggerkt.FileWriter
+
 import com.serebit.loggerkt.Logger
+import com.serebit.loggerkt.writers.FileWriter
 import io.kotlintest.TestCaseContext
 import io.kotlintest.matchers.shouldBe
 import io.kotlintest.specs.StringSpec
 import java.io.File
 
 class FileWriterTest : StringSpec() {
-    private val fileName = "text.txt"
-    private val file = File("${FileWriter::class.java.protectionDomain.codeSource.location.toURI().path}/$fileName")
+    private val file = File("/tmp/test.tmp")
 
     init {
-        "File contents should equal input string" {
+        "Logger should log messages to the set file" {
             Logger.info("My name jeff")
             file.readText().trim() shouldBe "INFO: My name jeff"
+        }
+
+        "Logger should log multiple messages to the same file" {
+            Logger.info("Test string")
+            Logger.info("Second test string")
+            file.readText().trim() shouldBe "INFO: Test string\nINFO: Second test string"
         }
     }
 
     override fun interceptTestCase(context: TestCaseContext, test: () -> Unit) {
-        Logger.writer = FileWriter(fileName)
+        Logger.writer = FileWriter(file.absolutePath, false)
         Logger.format = { _, _, _, _, level, message ->
             "$level: $message"
         }
