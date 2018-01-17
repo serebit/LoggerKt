cce66ad7d2d0ec59e5a79a2c66447071474577f2
Campbell Jones <serebitmail@gmail.com>
2018-01-16 19:14:45 -0500

    Added MultiWriter, added overwrite property to FileWriter

--- src/main/kotlin/com/serebit/loggerkt/writers/LogWriters.kt
+++ src/main/kotlin/com/serebit/loggerkt/writers/LogWriters.kt
@@ -14,24 +14,35 @@ interface LogWriter {
  *
  * @param path The path to the log file.
  */
-class FileWriter(path: String) : LogWriter {
+class FileWriter(path: String, overwrite: Boolean = true) : LogWriter {
     private val file = if (path.startsWith("/")) {
         File(path)
     } else {
         File(this::class.java.protectionDomain.codeSource.location.toURI().path + "/" + path)
-    }.also { it.writeText("") }
+    }.also { if (overwrite) it.delete() }
 
     override fun log(level: LogLevel, message: String) = file.appendText("$message\n")
 }
 
 /**
- * An implementation of [LogWriter] that outputs to the console. The output of this LogWriter is color-coded based
- * on the log level, using ANSI color codes.
+ * An implementation of [LogWriter] that outputs to the console. This writer's output can be color-coded.
+ *
+ * @property useAnsiColors Defines whether or not the writer will output color-coded text to the console.
  */
 class ConsoleWriter(private val useAnsiColors: Boolean = true) : LogWriter {
-    override fun log(level: LogLevel, message: String) {
-        println(message.let {
-            if (useAnsiColors) it.let(level.ansiColorTransform)
-        })
+    override fun log(level: LogLevel, message: String) = if (useAnsiColors) {
+        println(message.let(level.ansiColorTransform))
+    } else {
+        println(message)
     }
+}
+
+/**
+ * An implementation of [LogWriter] that contains multiple LogWriters. This writer passes the log messages through to
+ * its internal writers, allowing for multiple log outputs.
+ *
+ * @property writers The writers that will log the messages passed to this writer.
+ */
+class MultiWriter(private vararg val writers: LogWriter) : LogWriter {
+    override fun log(level: LogLevel, message: String) = writers.forEach { it.log(level, message) }
 }
