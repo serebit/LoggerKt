4ca5715ea5c19ad8593e2dd951e14f521d548f4d
Campbell Jones <serebitmail@gmail.com>
2018-01-16 21:44:54 -0500

    Improved Logger tests and added test to make sure all log levels are being output

--- src/test/kotlin/LoggerTest.kt
+++ src/test/kotlin/LoggerTest.kt
@@ -1,4 +1,3 @@
-
 import com.serebit.loggerkt.LogLevel
 import com.serebit.loggerkt.Logger
 import com.serebit.loggerkt.writers.LogWriter
@@ -11,7 +10,7 @@ class LoggerTest : StringSpec() {
         "Logger should log messages" {
             "Test log string".let { message ->
                 Logger.info(message)
-                (Logger.writer as TestWriter).log shouldBe "INFO: $message"
+                (Logger.writer as TestWriter).log shouldBe "INFO: $message\n"
             }
         }
 
@@ -22,12 +21,29 @@ class LoggerTest : StringSpec() {
             (Logger.writer as TestWriter).log shouldBe ""
         }
 
+        "Logger shoud be able to log messages of all levels if set to do so" {
+            Logger.level = LogLevel.TRACE
+            Logger.trace("Test trace string")
+            Logger.debug("Test debug string")
+            Logger.info("Test info string")
+            Logger.warn("Test warning string")
+            Logger.error("Test error string")
+            (Logger.writer as TestWriter).log shouldBe """
+                TRACE: Test trace string
+                DEBUG: Test debug string
+                INFO: Test info string
+                WARNING: Test warning string
+                ERROR: Test error string
+
+            """.trimIndent()
+        }
+
         "Logger should output correct class and function" {
             Logger.format = { _, _, className, method, _, _ ->
                 "$className.$method"
             }
             functionForTest()
-            (Logger.writer as TestWriter).log shouldBe "LoggerTest.functionForTest"
+            (Logger.writer as TestWriter).log shouldBe "LoggerTest.functionForTest\n"
         }
     }
 
@@ -47,7 +63,7 @@ class LoggerTest : StringSpec() {
             private set
 
         override fun log(level: LogLevel, message: String) {
-            log += message
+            log += "$message\n"
         }
     }
 }
