e310dab92f979c55566ae4f078bc41bc26f4ba1c
Campbell Jones <me@serebit.com>
2018-08-06 22:51:20 -0400

    Added a native subproject for later use.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,12 +3,21 @@ enableFeaturePreview("STABLE_PUBLISHING")
 
 include(":common")
 include(":jvm")
+include(":native")
 
 pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://dl.bintray.com/jetbrains/kotlin-native-dependencies")
+    }
+
     resolutionStrategy {
         eachPlugin {
-            if (requested.id.id.startsWith("kotlin-platform-")) {
-                useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
+            when {
+                requested.id.id.startsWith("kotlin-platform-") ->
+                    useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
+                requested.id.id == "konan" ->
+                    useModule("org.jetbrains.kotlin:kotlin-native-gradle-plugin:${requested.version}")
             }
         }
     }
