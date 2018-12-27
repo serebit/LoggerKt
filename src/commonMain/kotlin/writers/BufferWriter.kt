1129080724285820de6ffda5ac20763a695c8c5d
Campbell Jones <me@serebit.com>
2018-12-27 12:33:52 -0500

    Added BufferWriter

---
+++ src/commonMain/kotlin/writers/BufferWriter.kt
@@ -0,0 +1,21 @@
+package com.serebit.logkat.writers
+
+import com.serebit.logkat.LogLevel
+import com.serebit.logkat.platform.Platform
+
+/**
+ * An implementation of [MessageWriter] that writes to a string buffer. The chronological order of log entries is not
+ * guaranteed.
+ */
+class BufferWriter : MessageWriter {
+    private val buffer = StringBuilder()
+
+    override fun write(message: String, level: LogLevel) {
+        buffer.append("$message${Platform.lineSeparator}")
+    }
+
+    /**
+     * Reads from the buffer to a String.
+     */
+    fun read() = buffer.toString()
+}
