29bbddde3be2c06a911714428db418bd2824ec39
Campbell Jones <git@serebit.com>
2019-11-12 15:34:13 -0500

    Attempt to fix weird publication issue with versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,11 @@ plugins {
     `maven-publish`
 }
 
-group = "com.serebit.logkat"
-version = System.getenv("SNAPSHOT_VERSION") ?: "0.5.0-SNAPSHOT"
-description = "A lightweight and simple Kotlin logger"
+allprojects {
+    group = "com.serebit.logkat"
+    version = System.getenv("SNAPSHOT_VERSION") ?: "0.5.0-SNAPSHOT"
+    description = "A lightweight and simple Kotlin logger"
+}
 
 repositories {
     mavenCentral()
