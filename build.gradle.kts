738507c6a7b879228f26db867dd50118eb065c5e
Campbell Jones <git@serebit.com>
2019-11-12 14:52:32 -0500

    Update plugins and dependencies, restructure buildSrc, add snapshot repo and Maven configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,12 @@ import com.serebit.logkat.buildsrc.*
 
 plugins {
     kotlin("multiplatform") version "1.3.50"
-    id("com.github.ben-manes.versions") version "0.25.0"
+    id("com.github.ben-manes.versions") version "0.27.0"
     `maven-publish`
 }
 
-group = ProjectInfo.group
-version = ProjectInfo.version
+group = "com.serebit.logkat"
+version = "0.5.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -17,7 +17,7 @@ repositories {
 kotlin {
     sourceSets.commonMain.get().dependencies {
         implementation(kotlin("stdlib-common"))
-        api(group = "com.soywiz.korlibs.klock", name = "klock", version = Versions.KLOCK)
+        api(group = "com.soywiz.korlibs.klock", name = "klock", version = "1.7.5")
     }
     sourceSets.commonTest.get().dependencies {
         implementation(kotlin("test-common"))
@@ -30,7 +30,7 @@ kotlin {
         }
         compilations["test"].defaultSourceSet.dependencies {
             implementation(kotlin("test-junit5"))
-            implementation(group = "org.junit.jupiter", name = "junit-jupiter", version = Versions.JUPITER)
+            implementation(group = "org.junit.jupiter", name = "junit-jupiter", version = "5.5.2")
         }
     }
 
@@ -39,4 +39,14 @@ kotlin {
 
 tasks.withType<Test> { useJUnitPlatform() }
 
-publishing.createBintrayRepository(System.getenv("BINTRAY_KEY"))
+publishing {
+    createBintrayRepositories()
+
+    val javadocJar by jarTask()
+    val sourcesJar by jarTask()
+
+    publishing.publications.withType<MavenPublication>().all {
+        // configure additional POM data for Maven Central
+        configureForMavenCentral(javadocJar, sourcesJar)
+    }
+}
--- buildSrc/src/Extensions.kt
+++ buildSrc/src/Extensions.kt
@@ -1,20 +1,85 @@
 package com.serebit.logkat.buildsrc
 
+import groovy.util.Node
+import org.gradle.api.Project
 import org.gradle.api.artifacts.repositories.MavenArtifactRepository
 import org.gradle.api.publish.PublishingExtension
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.jvm.tasks.Jar
+import org.gradle.kotlin.dsl.creating
 import org.gradle.kotlin.dsl.maven
 import org.jetbrains.kotlin.gradle.plugin.KotlinDependencyHandler
+
+private const val projectName = "logkat"
+private const val projectDescription = "A lightweight and simple Kotlin logger"
+private val projectDevelopers = listOf("serebit")
 
 fun KotlinDependencyHandler.api(group: String, name: String, version: String) = api("$group:$name:$version")
 fun KotlinDependencyHandler.implementation(group: String, name: String, version: String) =
     implementation("$group:$name:$version")
 
-fun PublishingExtension.createBintrayRepository(accessKey: String?): MavenArtifactRepository =
-    repositories.maven("https://api.bintray.com/maven/serebit/public/${ProjectInfo.name}/;publish=0") {
-        name = "bintray"
+fun PublishingExtension.createBintrayRepositories() {
+    fun MavenArtifactRepository.applyCredentials() = credentials {
+        username = "serebit"
+        System.getenv("BINTRAY_KEY")?.let { password = it }
+    }
 
-        credentials {
-            username = "serebit"
-            accessKey?.let { password = it }
+    // create public
+    repositories.maven("https://api.bintray.com/maven/serebit/public/$projectName/;publish=0") {
+        name = "public"
+        applyCredentials()
+    }
+
+    // create snapshot
+    repositories.maven("https://api.bintray.com/maven/serebit/snapshot/$projectName/;publish=1") {
+        name = "snapshot"
+        applyCredentials()
+    }
+}
+
+fun Project.jarTask() = tasks.creating(Jar::class) {
+    archiveClassifier.value(name.removeSuffix("Jar"))
+}
+
+private fun Node.add(key: String, value: String) = appendNode(key).setValue(value)
+private inline fun Node.node(key: String, content: Node.() -> Unit) = appendNode(key).also(content)
+
+fun MavenPublication.configureForMavenCentral(javadocJar: Jar, sourcesJar: Jar) {
+    artifact(javadocJar)
+    if (name == "kotlinMultiplatform") artifact(sourcesJar)
+
+    pom.withXml {
+        asNode().run {
+            add("name", projectName)
+            add("description", projectDescription)
+            add("url", "https://gitlab.com/serebit/$projectName")
+            node("organization") {
+                add("name", "com.serebit")
+                add("url", "https://serebit.com")
+            }
+            node("issueManagement") {
+                add("system", "gitlab")
+                add("url", "https://gitlab.com/serebit/$projectName/issues")
+            }
+            node("licenses") {
+                node("license") {
+                    add("name", "Apache License 2.0")
+                    add("url", "https://gitlab.com/serebit/$projectName/blob/master/LICENSE.md")
+                    add("distribution", "repo")
+                }
+            }
+            node("scm") {
+                add("url", "https://gitlab.com/serebit/$projectName")
+                add("connection", "scm:git:git://gitlab.com/serebit/$projectName.git")
+                add("developerConnection", "scm:git:ssh://gitlab.com/serebit/$projectName.git")
+            }
+            node("developers") {
+                projectDevelopers.forEach {
+                    node("developer") {
+                        add("name", it)
+                    }
+                }
+            }
         }
     }
+}
